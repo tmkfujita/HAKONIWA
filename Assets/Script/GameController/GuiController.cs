205c984884c103e39c2a0770f1a308afe7feac2c
tmkfujita <seamist327@gmail.com>
2015-12-31 01:45:40 +0900

    2nd version upload

---
+++ Assets/Script/GameController/GuiController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiController : MonoBehaviour {
+
+    private Texture viewCenterTexture;
+    private Rect viewCenterRectangle;
+
+	// Use this for initialization
+	void Start () {
+        int image_width= Screen.width/15;
+        int image_height= image_width;
+        int image_x = Screen.width / 2 - image_width / 2;
+        int image_y = Screen.height / 2 - image_height / 2;
+
+        viewCenterTexture = (Texture)Resources.Load("Images/hand_b");
+        viewCenterRectangle = new Rect(image_x,image_y,image_width,image_height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setViewCenterImage(string imagePath)
+    {
+        viewCenterTexture = (Texture)Resources.Load(imagePath);
+    }
+
+    void OnGUI()
+    {
+        updateViewCenterImage();
+        updateViewLabels();
+    }
+
+    private void updateViewCenterImage()
+    {
+
+        GUI.DrawTexture(viewCenterRectangle, viewCenterTexture);
+    }
+
+    private void updateViewLabels()
+    {
+
+    }
+}
---
+++ Assets/Script/GameController/ViewController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewController : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public float cameraDetectDist=10;
+    public float clickableDist = 1.0f;
+
+
+    private GuiController guiContorller;
+    private Camera playerCamera;
+    private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+        try
+        {
+            guiContorller =GetComponent<GuiController>();
+
+            if (mainCamera != null)
+            {
+                playerCamera = mainCamera.GetComponent<Camera>();
+            }
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("error: player camera is not detected");
+            throw;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool mouseLeftButton = Input.GetMouseButtonUp(0);
+        bool mouseRightButton = Input.GetMouseButtonUp(1);
+
+        if (playerCamera != null)
+        {
+            Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
+            if(Physics.Raycast(ray, out hit, cameraDetectDist))
+            {
+                Transform objectHit = hit.transform;
+                //Debug.Log("distance(" + objectHit.tag + ")" + hit.distance);
+                if (hit.distance < clickableDist)
+                {
+                    guiContorller.setViewCenterImage("Images/hand_r");
+                    //Debug.Log("clickable mode on");
+                    if (mouseLeftButton)
+                    {
+                        Debug.Log("grep objecct->" + objectHit.tag);
+                    }
+                }
+                else
+                {
+                    guiContorller.setViewCenterImage("Images/hand_b");
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction * 20, Color.yellow);
+
+        }
+	}
+}
