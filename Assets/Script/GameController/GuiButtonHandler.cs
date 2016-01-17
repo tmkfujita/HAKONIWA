2abc5f33719907f76c5088e6cb3dfad7291816b9
tmkfujita <fkaproject@gmail.com>
2016-01-17 17:00:28 +0900

    GUI関連の構造整理

--- Assets/Script/GameController/GuiButtonHandler.cs
+++ Assets/Script/GameController/GuiButtonHandler.cs
@@ -5,25 +5,62 @@ public class GuiButtonHandler : MonoBehaviour {
 
     private ViewController viewContoroller;
     private GuiController guiController;
+    private Canvas menuOverlayCanvas;
+
+    private bool gameEndCheckerFlg = false;
 
 	// Use this for initialization
 	void Start () {
         viewContoroller = GameObject.Find("RigidBodyFPSController").GetComponent<ViewController>();
-        guiController = viewContoroller.getGuiController();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        //guiController = viewContoroller.getGuiController();
+        menuOverlayCanvas = GameObject.Find("menuOverlayCanvas").GetComponent<Canvas>();
+        menuOverlayCanvas.planeDistance = 30;
+    }
 
     public void click_gameEndButton()
     {
+        if (gameEndCheckerFlg) return;
+
         Debug.Log("game end button clicked");
+        //set dialogue
+        gameEndCheckerFlg = true;
+        dialogueCanvasOnOff(true);
     }
+
+    public void click_gameEndButton_OK()
+    {
+        Debug.Log("game stop->"+ gameEndCheckerFlg);
+        if (!gameEndCheckerFlg) return;
+        gameEndCheckerFlg = false;
+
+        //tmp
+        dialogueCanvasOnOff(true);
+    }
+    public void click_gameEndButton_Cancel()
+    {
+        Debug.Log("canceled->"+gameEndCheckerFlg);
+        if (!gameEndCheckerFlg) return;
+        gameEndCheckerFlg = false;
+        dialogueCanvasOnOff(false);
+    }
+
+    public void dialogueCanvasOnOff(bool dialogueOn)
+    {
+        if (dialogueOn == true)
+        {
+            menuOverlayCanvas.planeDistance = 12;
+        }
+        else
+        {
+            menuOverlayCanvas.planeDistance = 30;
+        }
+    }
+
     public void click_menuCloseButton()
     {
-        Debug.Log("menu close button clicked");
+        if (gameEndCheckerFlg) return;
 
+        Debug.Log("menu close button clicked");
+        viewContoroller.setMenuWindowOff();
     }
 }
--- Assets/Script/GameController/ViewController.cs
+++ Assets/Script/GameController/ViewController.cs
@@ -2,49 +2,59 @@
 using System.Collections;
 using UnityStandardAssets.Characters.FirstPerson;
 
+//===================================================
+//This Class is the main controller for HAKONIWA game
+//what is checking every interaction of game objects.
+//===================================================
+
 public class ViewController : MonoBehaviour {
 
-    public Camera playerCamera;
-    public bool menuModeFlg = false;
+    private RigidbodyFirstPersonController rigidBodyFPSController;
+    private GuiController guiContorller;//rendering controller of game items
+    private RaycastHit hit;
+    private bool menuModeFlg = false;
+    private ArrayList gameControllerObjects = new ArrayList();
+
+    public Camera playerCamera;//camera to render player FPS view
     public float cameraDetectDist=10;
     public float clickableDist = 1.0f;
 
-    private GuiController guiContorller;
-    private RaycastHit hit;
-
-    private RigidbodyFirstPersonController rigidBodyFPSController;
-
-	// Use this for initialization
+	// initialize all controllers
 	void Start () {
-        try
-        {
-            guiContorller = new GuiController();
 
-            if (playerCamera != null)
-            {
-                guiContorller.initialize(playerCamera);
-            }
-
-            rigidBodyFPSController = GameObject.Find("RigidBodyFPSController").GetComponent<RigidbodyFirstPersonController>();
-        }
-        catch (System.Exception)
+        if (playerCamera == null)
         {
-            Debug.Log("error: player camera is not detected");
-            throw;
+            Debug.Log("ERROR: player camera is not set");
+            return;
         }
+
+        rigidBodyFPSController = GameObject.Find("RigidBodyFPSController").GetComponent<RigidbodyFirstPersonController>();
+        guiContorller = new GuiController();
+        guiContorller.initialize(playerCamera);
+
     }
 	
-	// Update is called once per frame
+	// (main routine) all controllers are controlled here
 	void Update () {
 
+        if (playerCamera == null)
+        {
+            Debug.Log("ERROR: player camera is not set");
+            return;
+        }
+
+        //check user input events
         bool mouseLeftButton = Input.GetMouseButtonUp(0);
         bool mouseRightButton = Input.GetMouseButtonUp(1);
 
-        if (mouseRightButton)
-        {
-            triggerMenuMode();
-        }
+        //====================================
+        //*** GAME ENVIRONMENT CHECK START ***
+        //====================================
 
+        //menu mode controller is executed when user touch right mouse button
+        if (mouseRightButton==true) changeMenuMode();
+
+        //control cursor mode on the game window
         if (menuModeFlg==true)
         {
             UnityEngine.Cursor.visible = true;
@@ -56,48 +66,73 @@ public class ViewController : MonoBehaviour {
             UnityEngine.Cursor.visible = false;
             UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         }
-        if (playerCamera != null)
-        {
-            checkFrontObject(mouseLeftButton);
-        }
-	}
 
+        //====================================
+        //*** GAME OBJECT CHECK START ***
+        //====================================
+
+        GameObject detectedGameObject = null;
+
+        //check game object is clicked in front of the player
+        detectedGameObject = checkFrontObject(mouseLeftButton);
+        if(detectedGameObject != null)
+        {
+            //Debug.Log("check routine activated ->" + detectedGameObject.tag);
+            
+            //check object type
+
+            //if it is a Item, get name and destroy Gameobject
+        }
+
+
+
+    }
+
+    //gui controller
     void OnGUI()
     {
         guiContorller.controlGui();
     }
 
-    private void triggerMenuMode()
+    //I'll probably use this method later
+    private void checkGameControlObjects()
     {
-        if (menuModeFlg == true)
+        foreach (GameObject obj in gameControllerObjects)
         {
-            setMenuModeOff();
-        }
-        else
-        {
-            setMenuModeOn();
+
         }
     }
 
-    public void setMenuModeOn()
+    //menu mode is changed when user click the mouse right button
+    private void changeMenuMode()
     {
-        //Debug.Log("menu mode on");
+        if (menuModeFlg == true)
+        {
+            setMenuWindowOff();
+        }
+        else
+        {
+            setMenuWindowOn();
+        }
+    }
+    
+    public void setMenuWindowOn()
+    {
         UnityEngine.Cursor.lockState = CursorLockMode.Confined;
         menuModeFlg = true;
         rigidBodyFPSController.menuModeFlg = true;
         //set gui console mode to defaultMenu mode
         guiContorller.setMenuConsoleMode(1);
     }
-    public void setMenuModeOff()
+    public void setMenuWindowOff()
     {
-        //Debug.Log("menu mode off");
         menuModeFlg = false;
         rigidBodyFPSController.menuModeFlg = false;
         guiContorller.setMenuConsoleMode(0);
     }
-
+    
    //control detected object
-    private void checkFrontObject(bool mouseLeftButton)
+    private GameObject checkFrontObject(bool mouseLeftButton)
 
     {
         string imagePath = "Images/UI_Touch";
@@ -110,12 +145,7 @@ public class ViewController : MonoBehaviour {
             if (hit.distance < clickableDist)
             {
                 guiContorller.setViewCenterImage(imagePath);
-
-                //Debug.Log("clickable mode on:"+ mouseLeftButton);
-                if (mouseLeftButton)
-                {
-                    Debug.Log("grep objecct->" + objectHit.tag);
-                }
+                if (mouseLeftButton) return objectHit.gameObject;
             }
             else
             {
@@ -123,11 +153,16 @@ public class ViewController : MonoBehaviour {
                 guiContorller.setViewCenterImage("");
             }
         }
-        Debug.DrawRay(ray.origin, ray.direction * 20, Color.yellow);
+        //Debug.DrawRay(ray.origin, ray.direction * 20, Color.yellow);
+        return null;
     }
 
+    //===============================================
+    //following methods are getter/setter
+    //===============================================
     public GuiController getGuiController()
     {
         return guiContorller;
     }
+    
 }
