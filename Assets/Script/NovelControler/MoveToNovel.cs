1821117027a0c176cd24901c76912d71e24113b0
tkg <takagiyuuye@TKGMBA.local>
2016-01-30 10:00:04 +0900

    起動時の解像度設定

--- Assets/Script/NovelControler/MoveToNovel.cs
+++ Assets/Script/NovelControler/MoveToNovel.cs
@@ -6,7 +6,8 @@ public class MoveToNovel : MonoBehaviour
 
 	public void MoveToNovelFunc () {
 		Debug.Log ("MoveToNovelFunc");
-		Application.LoadLevel("ADV");
+//		Application.LoadLevel("ADV");
+		UnityEngine.SceneManagement.SceneManager.LoadScene("hakoniwa_game");
 
 	}
 
